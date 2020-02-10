76c33ccb9ec5a5ee695cbeebd34d565b9e05108f
Michael Lemley <michael.lemley@coinninja.com>
2020-02-10 17:55:22 -0600

    have home view fragment observe state changes

--- app/src/main/java/app/lemley/crypscape/ui/home/HomeViewModel.kt
+++ app/src/main/java/app/lemley/crypscape/ui/home/HomeViewModel.kt
@@ -3,11 +3,37 @@ package app.lemley.crypscape.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import app.lemley.crypscape.ui.base.Action
+import app.lemley.crypscape.ui.base.BaseViewModel
+import app.lemley.crypscape.ui.base.Event
+import app.lemley.crypscape.ui.base.Result
+import app.lemley.crypscape.ui.base.State
+import app.lemley.crypscape.usecase.UseCase
+import kotlinx.coroutines.flow.Flow
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel : BaseViewModel<HomeViewModel.Events, HomeViewModel.HomeState>() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    sealed class Events: Event {
+
     }
-    val text: LiveData<String> = _text
+
+    data class HomeState(
+        val foo:Boolean = false
+    ): State
+
+    override val useCases: List<UseCase>
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+    override fun makeInitState(): HomeState {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun Flow<Events>.eventTransform(): Flow<Action> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun HomeState.plus(result: Result): HomeState {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
