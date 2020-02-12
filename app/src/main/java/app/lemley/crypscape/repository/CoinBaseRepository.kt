bcc3a6c53d4970826eaac9f412f5be28d09634da
Michael Lemley <michael.lemley@coinninja.com>
2020-02-12 14:44:44 -0600

    wip

--- app/src/main/java/app/lemley/crypscape/repository/CoinBaseRepository.kt
+++ app/src/main/java/app/lemley/crypscape/repository/CoinBaseRepository.kt
@@ -1,7 +1,8 @@
 package app.lemley.crypscape.repository
 
 
-class CoinBaseRepository()  {
+class CoinBaseRepository(
+)  {
     fun syncProducts() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
