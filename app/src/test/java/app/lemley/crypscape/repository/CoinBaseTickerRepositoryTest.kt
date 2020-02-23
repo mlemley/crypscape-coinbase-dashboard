60c3d7461c9d75f19ea1fddfc856ead2886b70d9
Michael Lemley <michael.lemley@coinninja.com>
2020-02-23 10:39:26 -0600

    implement fetch of ticker for given product id

---
+++ app/src/test/java/app/lemley/crypscape/repository/CoinBaseTickerRepositoryTest.kt
@@ -0,0 +1,16 @@
+package app.lemley.crypscape.repository
+
+import org.junit.Test
+
+import com.google.common.truth.Truth.assertThat
+
+class CoinBaseTickerRepositoryTest {
+
+    @Test
+    fun example() {
+
+        assertThat(false).isTrue()
+    }
+
+
+}
