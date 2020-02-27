47c5cc4e11024b772a87759695fdf79b18f8de78
Michael Lemley <michael.lemley@coinninja.com>
2020-02-27 17:38:55 -0600

    spike out scarlet

--- app/src/main/java/app/lemley/crypscape/client/coinbase/CoinBaseApiFactory.kt
+++ app/src/main/java/app/lemley/crypscape/client/coinbase/CoinBaseApiFactory.kt
@@ -1,19 +1,27 @@
 package app.lemley.crypscape.client.coinbase
 
+import android.app.Application
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.tinder.scarlet.Scarlet
+import com.tinder.scarlet.lifecycle.android.AndroidLifecycle
+import com.tinder.scarlet.messageadapter.gson.GsonMessageAdapter
+import com.tinder.scarlet.websocket.okhttp.newWebSocketFactory
+import com.tinder.streamadapter.coroutines.CoroutinesStreamAdapterFactory
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
 object CoinBaseApiFactory {
     private const val apiBaseUrl = "https://api.pro.coinbase.com"
+    private const val wsFeedUrl = "wss://ws-feed.pro.coinbase.com"
 
     private val coinBaseClient = OkHttpClient().newBuilder()
         // add interceptors here
         .build()
 
-    private fun retrofit(baseUrl:String): Retrofit = Retrofit.Builder()
+    private fun retrofit(baseUrl: String): Retrofit = Retrofit.Builder()
         .baseUrl(baseUrl)
         .client(coinBaseClient)
         .addConverterFactory(GsonConverterFactory.create())
@@ -26,4 +34,23 @@ object CoinBaseApiFactory {
                 baseUrl
             ).create(CoinBaseApi::class.java)
         )
+
+
+    fun coinBaseWSClient(application: Application): CoinBaseWSService {
+        val scarlet = Scarlet.Builder()
+            .webSocketFactory(webSocketOkHttpClient().newWebSocketFactory(wsFeedUrl))
+            .lifecycle(AndroidLifecycle.ofApplicationForeground(application))
+            .addMessageAdapterFactory(GsonMessageAdapter.Factory())
+            .addStreamAdapterFactory(CoroutinesStreamAdapterFactory())
+            .build()
+        return scarlet.create()
+    }
+
+    private fun webSocketOkHttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor.apply {
+                httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            }).build()
+    }
 }
--- app/src/main/java/app/lemley/crypscape/usecase/MarketDataUseCase.kt
+++ app/src/main/java/app/lemley/crypscape/usecase/MarketDataUseCase.kt
@@ -1,16 +1,21 @@
 package app.lemley.crypscape.usecase
 
+import android.util.Log
+import app.lemley.crypscape.client.coinbase.CoinBaseWSService
+import app.lemley.crypscape.client.coinbase.model.Subscribe
 import app.lemley.crypscape.client.coinbase.model.Ticker
+import app.lemley.crypscape.client.coinbase.model.subscriptionFor
 import app.lemley.crypscape.extensions.exhaustive
 import app.lemley.crypscape.model.MarketConfiguration
-import app.lemley.crypscape.persistance.entities.Candle
 import app.lemley.crypscape.persistance.entities.Granularity
 import app.lemley.crypscape.repository.CoinBaseRepository
 import app.lemley.crypscape.repository.DefaultMarketDataRepository
 import app.lemley.crypscape.ui.base.Action
 import app.lemley.crypscape.ui.base.Result
+import com.tinder.scarlet.WebSocket
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.emptyFlow
@@ -19,7 +24,8 @@ import kotlinx.coroutines.flow.flowOn
 @ExperimentalCoroutinesApi
 class MarketDataUseCase constructor(
     val defaultMarketDataRepository: DefaultMarketDataRepository,
-    val coinBaseRepository: CoinBaseRepository
+    val coinBaseRepository: CoinBaseRepository,
+    val coinBaseWSService: CoinBaseWSService
 ) : UseCase {
 
     sealed class MarketActions : Action {
@@ -45,13 +51,39 @@ class MarketDataUseCase constructor(
         }.exhaustive
     }
 
-    private fun handleOnGranularityChanged(granularity: Granularity): Flow<Result> = channelFlow<Result> {
-        val marketConfiguration = defaultMarketDataRepository.changeGranularity(granularity)
-        send(MarketResults.MarketConfigurationResult(marketConfiguration))
-        coinBaseRepository.tickerForConfiguration(marketConfiguration)?.let {
-            send(MarketResults.TickerResult(it))
-        }
-    }.flowOn(Dispatchers.IO)
+    private fun handleOnGranularityChanged(granularity: Granularity): Flow<Result> =
+        channelFlow<Result> {
+            val marketConfiguration = defaultMarketDataRepository.changeGranularity(granularity)
+            send(MarketResults.MarketConfigurationResult(marketConfiguration))
+            coinBaseRepository.tickerForConfiguration(marketConfiguration)?.let {
+                send(MarketResults.TickerResult(it))
+            }
+            val subscribeMessage = subscriptionFor(
+                type = Subscribe.Type.Subscribe,
+                products = listOf("BTC-USD"),
+                channels = listOf(Subscribe.Channel.Ticker)
+            )
+
+            coinBaseWSService.observeWebSocketEvent()
+                .consumeEach {
+                    when (it) {
+                        is WebSocket.Event.OnConnectionOpened<*> -> {
+                            Log.w("--websocket--", "connection opened")
+                            coinBaseWSService.sendSubscribe(subscribeMessage)
+                            Log.w("--websocket--", "subscribe")
+                            Log.w("--websocket--", "observe ticker changes")
+                            coinBaseWSService.observeTicker().consumeEach { ticker ->
+                                Log.e(
+                                    "-- websocket --",
+                                    "Bitcoin price is ${ticker.price} at ${ticker.time}"
+                                )
+
+                                send(MarketResults.TickerResult(ticker))
+                            }
+                        }
+                    }
+                }
+        }.flowOn(Dispatchers.IO)
 
     private fun handleFetchDefaultMarketData(): Flow<Result> = channelFlow<Result> {
         val marketConfiguration = defaultMarketDataRepository.loadDefault()
