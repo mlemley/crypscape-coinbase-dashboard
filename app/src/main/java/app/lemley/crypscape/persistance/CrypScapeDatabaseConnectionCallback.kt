00a0cde9108674bcbc234109397c634cde7e6c28
Michael Lemley <mlemleyap@gmail.com>
2020-03-10 10:52:08 -0500

    update assignment to be millis

--- app/src/main/java/app/lemley/crypscape/persistance/CrypScapeDatabaseConnectionCallback.kt
+++ app/src/main/java/app/lemley/crypscape/persistance/CrypScapeDatabaseConnectionCallback.kt
@@ -1,13 +1,8 @@
 package app.lemley.crypscape.persistance
 
-import androidx.annotation.WorkerThread
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import app.lemley.crypscape.extensions.toInstant
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.threeten.bp.Instant
 
 class CrypScapeDatabaseConnectionCallback : RoomDatabase.Callback() {
@@ -18,10 +13,12 @@ class CrypScapeDatabaseConnectionCallback : RoomDatabase.Callback() {
     }
 
     private fun populateInitialPlatforms(db: SupportSQLiteDatabase) {
-        val startDate = "2017-12-01T00:00Z".toInstant() ?: Instant.now()
-        db.execSQL("""
+        val startDate = ("2017-12-01T00:00Z".toInstant() ?: Instant.now()).toEpochMilli()
+        db.execSQL(
+            """
             insert into `platform`(name, startDate)  
-            values ("Coinbase Pro", ${startDate.toEpochMilli()})
-        """)
+            values ("Coinbase Pro", $startDate)
+        """
+        )
     }
 }
