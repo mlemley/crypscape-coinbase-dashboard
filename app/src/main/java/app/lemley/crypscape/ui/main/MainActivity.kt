0c92309e2c434b24bc9f804c74428c2f0a9bde47
Michael Lemley <mlemleyap@gmail.com>
2020-03-19 14:13:50 -0500

    Feature/bottom navigation (#15)

--- app/src/main/java/app/lemley/crypscape/ui/main/MainActivity.kt
+++ app/src/main/java/app/lemley/crypscape/ui/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package app.lemley.crypscape.ui.main
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -8,18 +9,21 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import app.lemley.crypscape.R
+import app.lemley.crypscape.databinding.ActivityMainBinding
 import app.lemley.crypscape.extensions.app.withView
-import com.google.android.material.navigation.NavigationView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
 
     private val configuration: Configuration by inject()
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binder: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binder = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binder.root)
         setSupportActionBar(findViewById(R.id.toolbar))
         appBarConfiguration = configuration.configureWithDrawer(withView(R.id.drawer_layout))
         findNavController(R.id.nav_host_fragment).apply {
@@ -30,7 +34,8 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             setupActionBarWithNavController(this, appBarConfiguration)
-            withView<NavigationView>(R.id.navigation).setupWithNavController(this)
+            binder.navigation.setupWithNavController(this)
+            withView<BottomNavigationView>(R.id.bottom_navigation).setupWithNavController(this)
         }
     }
 
